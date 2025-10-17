668f06a99a1fe54e5655f70808f2fa04e37f1500
Teppi-txt <teppipersonal@gmail.com>
2025-10-16 22:06:53 -0400

    Fixed caching and main methods

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,16 +13,24 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
+    private Map<String, List<String>> breedCache = new HashMap<>();
+    private final BreedFetcher fetcher;
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        if (breedCache.containsKey(breed)) {
+            return breedCache.get(breed);
+        }
+        callsMade += 1;
+        List<String> fetched = fetcher.getSubBreeds(breed);
+        breedCache.put(breed, fetched);
+        return fetched;
     }
 
     public int getCallsMade() {
